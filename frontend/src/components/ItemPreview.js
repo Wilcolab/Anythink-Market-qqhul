2056e0cad10edef7bd5de538a10628e934e807d7
0211AB <abhaybajajit@gmail.com>
2022-08-09 00:28:00 +0530

    Image Placeholder Updated

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-import placeholder from '../imgs/placeholder.png'
+import placeholder from "../imgs/placeholder.png";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
